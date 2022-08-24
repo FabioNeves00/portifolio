1b7f8f5afb08429e72db04d80fa4e5eb36051f2e
Fabio Neves <fabio@pop-os.localdomain>
2022-08-24 19:52:00 -0300

    feat: Hard structure done

--- src/constants/Tech_Texts.ts
+++ src/constants/Tech_Texts.ts
@@ -4,6 +4,6 @@ export const TECHS_TITLE_TEXT = {
 }
 
 export const TECHS_DESCRIPTION_TEXT = {
-    pt: "Apesar de ter conhecimento em ambas as áreas da programação web, meu foco é majoritariamente o back end, com tecnologias como NestJS, Typescript e bancos de dados.",
-    en: "Despite having knowledge in both areas of web programming, my focus is mostly on the back end, with technologies like NestJS, Typescript and databases."
+    pt: "Apesar de ter conhecimento em ambas as áreas da programação web, meu foco é majoritariamente o back end, com tecnologias como NestJS, Typescript e bancos de dados baseados em documentos ou tabelas.",
+    en: "Despite having knowledge in both areas of web programming, my focus is mostly on the back end, with technologies like NestJS, Typescript and document or tables databases."
 }
